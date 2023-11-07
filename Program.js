8af2546c90253df79b197fea6228d0ce6863df3b
Nicolas Dodino <ND284408@fi365.ort.edu.uy>
2023-11-07 17:07:51 -0300

    as

--- Program.js
+++ Program.js
@@ -4,4 +4,6 @@ function divisionDeNumeros() {
    let numero1 = prompt("Ingresa el primer número:");
    var numero2 = prompt("Ingresa el segundo número:");
    console.log("El resultado de la división es: " + numero1 / numero2);
- }
+}
+
+divisionDeNumeros();
